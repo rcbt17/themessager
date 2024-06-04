776d22b39455362391de048e24ac1380679bc38a
Robin Ciubotaru <robinciubotaru17@gmail.com>
2024-06-04 14:41:39 +0300

    Database CRUD operations + Random message identifier in DB

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,28 @@
 class MessagesController < ApplicationController
+
+  before_action :generate_random_id, only: [:create]
+
   def new
     @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message[:randomuid] = @id
+    if @message.save
+      render json: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def generate_random_id
+    @id = SecureRandom.uuid.gsub(/[^0-9a-z ]/i,'')
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -1,23 +1,31 @@
 import { Controller } from "@hotwired/stimulus";
 
-
 // Connects to data-controller="message"
 export default class extends Controller {
-  static targets = ["xyRealTime", "randomKey", "content", "form", "submitBtn"];
+  static targets = [
+    "xyRealTime",
+    "randomKey",
+    "content",
+    "form",
+    "submitBtn",
+    "linkInput",
+  ];
   async connect() {
     this.contentTarget.disabled = true;
     this.submitBtnTarget.disabled = true;
+    this.submitBtnTarget.value = "Please wait for key generation...";
     let result = await this.generateRandomNumber();
     this.generatedKey = this.generateRandomKey(result); // Store the generated key in an instance variable
     this.contentTarget.disabled = false;
     this.submitBtnTarget.disabled = false;
+    this.submitBtnTarget.value = "Create Encrypted Note";
   }
 
   generateRandomNumber() {
     const randomsArray = [];
     const mouseMoveHandler = (e) => {
       this.xyRealTimeTarget.innerText = [e.clientX, e.clientY];
-      if (randomsArray.length < 20) {
+      if (randomsArray.length < 32) {
         randomsArray.push(e.clientX * e.clientY);
       }
     };
@@ -34,27 +42,28 @@ export default class extends Controller {
   }
 
   generateRandomKey(result) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let randomKey = '';
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let randomKey = "";
     let generatedRandomSeed = result.flat().reduce((acc, val) => acc + val, 0);
 
     const random = () => {
       const x = Math.sin(generatedRandomSeed++) * 10000;
       return x - Math.floor(x);
-    }
+    };
 
-    for (let i =0; i < 10; i++) {
+    for (let i = 0; i < 32; i++) {
       const randomIndex = Math.floor(random() * characters.length);
       randomKey += characters.charAt(randomIndex);
     }
     this.randomKeyTarget.innerText = randomKey;
-    this.randomKeyTarget.classList.add = 'font-weight-bold';
+    this.randomKeyTarget.classList.remove("text-muted");
+    this.randomKeyTarget.classList.add("font-weight-bold");
     return randomKey;
   }
 
-
-   encrypt(text, key) {
-    let encrypted = '';
+  encrypt(text, key) {
+    let encrypted = "";
     const base = 32; // Starting ASCII printable character code
     const range = 95; // Number of printable characters (126 - 32 + 1)
 
@@ -68,15 +77,16 @@ export default class extends Controller {
     return encrypted;
   }
 
-   decrypt(encrypted, key) {
-    let decrypted = '';
+  decrypt(encrypted, key) {
+    let decrypted = "";
     const base = 32;
     const range = 95;
 
     for (let i = 0; i < encrypted.length; i++) {
       const charCode = encrypted.charCodeAt(i);
       const keyCode = key.charCodeAt(i % key.length);
-      const current = ((charCode - base - (keyCode - base) + range) % range) + base;
+      const current =
+        ((charCode - base - (keyCode - base) + range) % range) + base;
       decrypted += String.fromCharCode(current);
     }
 
@@ -88,6 +98,46 @@ export default class extends Controller {
 
     const message = this.contentTarget.value;
     const finalMessage = this.encrypt(message, this.generatedKey);
-    this.formTarget.classList.add("d-none")
+    this.contentTarget.value = finalMessage;
+    const csrfToken = this.formTarget.querySelector(
+      'input[name="authenticity_token"]'
+    ).value;
+
+    const settingsPost = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "X-CSRF-Token": csrfToken,
+      },
+      body: JSON.stringify({
+        message: {
+          content: finalMessage,
+        },
+      }),
+    };
+
+    try {
+      const response = await fetch(this.formTarget.action, settingsPost);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      if (data.randomuid) {
+        this.formTarget.classList.add("d-none");
+        this.linkInputTarget.classList.remove("d-none");
+        this.randomKeyTarget.innerText = "Note created! Please copy and share the link below!";
+        this.linkInputTarget.value =
+          window.location.protocol +
+          "//" +
+          window.location.host +
+          "/messages/" +
+          data.randomuid +
+          "?secret=" +
+          this.generatedKey;
+      }
+    } catch (error) {
+      console.error("There was an error!", error);
+    }
   }
 }
