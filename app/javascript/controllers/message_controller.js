a2886712fef27ea1fa5653d421a80a2bc5b3d054
Robin Ciubotaru <robinciubotaru17@gmail.com>
2024-06-04 11:42:33 +0300

    Handling password encryption and form processing

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -1,13 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 
+
 // Connects to data-controller="message"
 export default class extends Controller {
-  static targets = ["xyRealTime", "randomKey"];
+  static targets = ["xyRealTime", "randomKey", "content", "form", "submitBtn"];
   async connect() {
+    this.contentTarget.disabled = true;
+    this.submitBtnTarget.disabled = true;
     let result = await this.generateRandomNumber();
-    let generatedKey = this.generateRandomKey(result);
-    console.log(generatedKey);
-
+    this.generatedKey = this.generateRandomKey(result); // Store the generated key in an instance variable
+    this.contentTarget.disabled = false;
+    this.submitBtnTarget.disabled = false;
   }
 
   generateRandomNumber() {
@@ -47,5 +50,44 @@ export default class extends Controller {
     this.randomKeyTarget.innerText = randomKey;
     this.randomKeyTarget.classList.add = 'font-weight-bold';
     return randomKey;
+  }
+
+
+   encrypt(text, key) {
+    let encrypted = '';
+    const base = 32; // Starting ASCII printable character code
+    const range = 95; // Number of printable characters (126 - 32 + 1)
+
+    for (let i = 0; i < text.length; i++) {
+      const charCode = text.charCodeAt(i);
+      const keyCode = key.charCodeAt(i % key.length);
+      const current = ((charCode - base + keyCode - base) % range) + base;
+      encrypted += String.fromCharCode(current);
+    }
+
+    return encrypted;
+  }
+
+   decrypt(encrypted, key) {
+    let decrypted = '';
+    const base = 32;
+    const range = 95;
+
+    for (let i = 0; i < encrypted.length; i++) {
+      const charCode = encrypted.charCodeAt(i);
+      const keyCode = key.charCodeAt(i % key.length);
+      const current = ((charCode - base - (keyCode - base) + range) % range) + base;
+      decrypted += String.fromCharCode(current);
+    }
+
+    return decrypted;
+  }
+
+  async encryptMessage(event) {
+    event.preventDefault();
+
+    const message = this.contentTarget.value;
+    const finalMessage = this.encrypt(message, this.generatedKey);
+    this.formTarget.classList.add("d-none")
   }
 }
